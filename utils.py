4d111294daff7acedd650f08488434ea7f036818
Vladislav Kuleykin <kzvdar42@gmail.com>
2019-11-17 03:32:22 +0300

    Added comparison of different face detection algorithms. Improved lazy image loader.

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import glob
 import cv2
 import json
 import pickle
+import re
 
 from tqdm import tqdm
 
@@ -34,3 +35,48 @@ def load_face_information(path):
     with open(path, 'rb') as f:
         res = pickle.load(f)
         return res
+
+
+class ImageLoader:
+    """Lazy image loader."""
+    def __init__(self, path, image_type='.jpg',
+                 return_full_path=True, sort_numerically=True):
+        self.image_paths = glob.glob(os.path.join(path, f'*{image_type}'))
+        if sort_numerically:
+            self.image_paths = sorted(self.image_paths, key=self.__num_keys)
+        self.return_full_path = return_full_path
+    
+    
+    def __num_keys(self, s):
+        """Key values for proper numerical sort of strings."""
+        key = []
+        for c in re.split('(\d+)', s):
+            key.append(int(c) if c.isdigit() else c)
+        return key
+            
+
+    def __len__(self):
+        return len(self.image_paths)
+
+
+    def __iter__(self):
+        for img_path in self.image_paths:
+            yield self.__return_path_and_img(img_path)
+
+
+    def __return_path_and_img(self, img_path):
+        # handle slices
+        if isinstance(img_path, list):
+            return [self.__return_path_and_img(path) for path in img_path]
+#             for path in img_path:
+#                 yield self.__return_path_and_img(path)
+        else:
+            img = load_image(img_path)
+            if not self.return_full_path:
+                img_path = os.path.split(img_path)[1]
+            return img_path, img
+
+
+    def __getitem__(self, ind):
+        img_path = self.image_paths[ind]
+        return self.__return_path_and_img(img_path)
